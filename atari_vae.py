606be8e352d34666fdfae6543bacb689d00f0022
Howard HUANG <hhuang63@CS.McGill.CA>
2018-10-23 14:06:13 -0400

    Successfully overfits on test data with latent forcing

--- atari_vae.py
+++ atari_vae.py
@@ -1,5 +1,4 @@
 import torch
-from torch.autograd import Variable
 import numpy as np
 import random
 import dill
@@ -7,6 +6,7 @@ import dill
 from PIL import Image
 from tqdm import tqdm
 
+LATENT_SIZE = 2048
 LATENT_SIZE = 2
 
 class ConvEncoder(torch.nn.Module):
@@ -45,6 +45,66 @@ class ConvEncoder(torch.nn.Module):
 
         return sample, mean, logvar
 
+class ConvEncoder(torch.nn.Module):
+    def __init__(self):
+        super(ConvEncoder, self).__init__()
+        self.relu = torch.nn.LeakyReLU()
+        #self.c1 = torch.nn.Conv2d(in_channels=3,out_channels=64,kernel_size=(8,8),stride=2,padding=(2,2))
+        #self.c2 = torch.nn.Conv2d(in_channels=64,out_channels=128,kernel_size=(6,6),stride=2,padding=(2,2))
+        #self.c3 = torch.nn.Conv2d(in_channels=128,out_channels=128,kernel_size=(6,6),stride=2,padding=(0,2))
+        #self.c4 = torch.nn.Conv2d(in_channels=128,out_channels=128,kernel_size=(4,4),stride=2)
+        self.c1 = torch.nn.Conv2d(in_channels=3,out_channels=64,kernel_size=(8,8),stride=2,padding=(0,1))
+        self.c2 = torch.nn.Conv2d(in_channels=64,out_channels=128,kernel_size=(6,6),stride=2,padding=(1,1))
+        self.c3 = torch.nn.Conv2d(in_channels=128,out_channels=128,kernel_size=(6,6),stride=2,padding=(1,1))
+        self.c4 = torch.nn.Conv2d(in_channels=128,out_channels=128,kernel_size=(4,4),stride=2)
+
+        self.fc1_mean = torch.nn.Linear(in_features=128*11*8,out_features=LATENT_SIZE,bias=True)
+        self.fc1_std = torch.nn.Linear(in_features=128*11*8,out_features=LATENT_SIZE,bias=True)
+
+    def forward(self, inputs, sample):
+        output = inputs.view(-1,3,210,160)
+        output = self.c1(output)
+        output = self.relu(output)
+        output = self.c2(output)
+        output = self.relu(output)
+        output = self.c3(output)
+        output = self.relu(output)
+        output = self.c4(output)
+        output = self.relu(output)
+        output = output.view(-1,128*11*8)
+
+        mean = self.fc1_mean(output)
+        logvar = self.fc1_std(output) # log(std^2)
+        std = logvar.mul(0.5).exp()
+
+        sample = sample.mul(std).add_(mean)
+
+        return sample, mean, logvar
+
+class ConvEncoder2(torch.nn.Module): # Linear
+    def __init__(self):
+        super(ConvEncoder, self).__init__()
+        self.relu = torch.nn.LeakyReLU()
+
+        #self.fc0 = torch.nn.Linear(in_features=3*210*160,out_features=500,bias=True)
+        #self.fc1_mean = torch.nn.Linear(in_features=500,out_features=LATENT_SIZE,bias=True)
+        #self.fc1_std = torch.nn.Linear(in_features=500,out_features=LATENT_SIZE,bias=True)
+        self.fc1_mean = torch.nn.Linear(in_features=3*210*160,out_features=LATENT_SIZE,bias=True)
+        self.fc1_std  = torch.nn.Linear(in_features=3*210*160,out_features=LATENT_SIZE,bias=True)
+
+    def forward(self, inputs, sample):
+        output = inputs.view(-1,3*210*160)
+        #output = self.fc0(output)
+        #output = self.relu(output)
+
+        mean = self.fc1_mean(output)
+        logvar = self.fc1_std(output) # log(std^2)
+        std = logvar.mul(0.5).exp()
+
+        sample = sample.mul(std).add_(mean)
+
+        return sample, mean, logvar
+
 class ConvDecoder(torch.nn.Module):
     def __init__(self):
         super(ConvDecoder, self).__init__()
@@ -73,6 +133,42 @@ class ConvDecoder(torch.nn.Module):
         output = self.c3(output)
         output = self.relu(output)
         output = self.c4(output)
+        output = self.sigmoid(output)
+
+        return output
+
+class ConvDecoder(torch.nn.Module):
+    def __init__(self):
+        super(ConvDecoder, self).__init__()
+        self.relu = torch.nn.LeakyReLU()
+        self.sigmoid = torch.nn.Sigmoid()
+
+        self.fc = torch.nn.Linear(in_features=LATENT_SIZE, out_features=128*11*8, bias=True)
+        self.fc = torch.nn.Linear(in_features=LATENT_SIZE,
+                out_features=3*210*160, bias=True)
+
+        #self.c1 = torch.nn.ConvTranspose2d(out_channels=128,in_channels=128,kernel_size=(4,4),stride=2,output_padding=(0,1))
+        #self.c2 = torch.nn.ConvTranspose2d(out_channels=128,in_channels=128,kernel_size=(6,6),stride=2,padding=(0,2),output_padding=(0,1))
+        #self.c3 = torch.nn.ConvTranspose2d(out_channels=128,in_channels=128,kernel_size=(6,6),stride=2,padding=(2,2),output_padding=(0,1))
+        #self.c4 = torch.nn.ConvTranspose2d(out_channels=3, in_channels=128,kernel_size=(8,8),stride=2,padding=(2,2),output_padding=(0,0))
+        self.c1 = torch.nn.ConvTranspose2d(out_channels=128,in_channels=128,kernel_size=(4,4),stride=2)
+        self.c2 = torch.nn.ConvTranspose2d(out_channels=128,in_channels=128,kernel_size=(6,6),stride=2,padding=(1,1))
+        self.c3 = torch.nn.ConvTranspose2d(out_channels=128,in_channels=128,kernel_size=(6,6),stride=2,padding=(1,1))
+        self.c4 = torch.nn.ConvTranspose2d(out_channels=3, in_channels=128,kernel_size=(8,8),stride=2,padding=(0,1))
+
+    def forward(self, inputs):
+        output = self.fc(inputs)
+        output = output.view(-1,3,210,160)
+        #output = self.relu(output)
+        #output = output.view(-1,128,11,8)
+
+        #output = self.c1(output)
+        #output = self.relu(output)
+        #output = self.c2(output)
+        #output = self.relu(output)
+        #output = self.c3(output)
+        #output = self.relu(output)
+        #output = self.c4(output)
         output = self.sigmoid(output)
 
         return output
@@ -110,9 +206,15 @@ class ConvDiscriminator(torch.nn.Module):
         return output
 
 def loss(in_img, out_img, mean, logvar):
+    #return torch.nn.functional.mse_loss(out_img.view(-1), in_img.view(-1))
     # Reconstruction loss
+    if not ((in_img >= 0.).all() and (in_img <= 1.).all()) or not ((out_img >= 0.).all() and (out_img <= 1.).all()):
+        print(in_img.size(), out_img.size())
+        print(torch.min(in_img), torch.max(in_img))
+        print(torch.min(out_img), torch.max(out_img))
+        assert False
     loss1 = torch.nn.functional.binary_cross_entropy(out_img.view(-1), in_img.view(-1))
-    #return loss1
+    return loss1
 
     # KL-divergence regularization
     # See https://stats.stackexchange.com/questions/7440/kl-divergence-between-two-univariate-gaussians?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
@@ -126,7 +228,7 @@ def loss(in_img, out_img, mean, logvar):
 
     # Total loss
     return loss1+loss2
-    
+
 def load_training_data():
     with open('atari.pkl', 'rb') as f:
         return dill.load(f)
@@ -141,10 +243,10 @@ test_data = load_testing_data()
 encoder = ConvEncoder().cuda()
 #decoder = Decoder().cuda()
 decoder = ConvDecoder().cuda()
-discriminator = ConvDiscriminator().cuda()
-optimizer = torch.optim.Adam(list(encoder.parameters())+list(decoder.parameters()), lr=1e-3)
+#discriminator = ConvDiscriminator().cuda()
+optimizer = torch.optim.Adam(list(encoder.parameters())+list(decoder.parameters()), lr=1e-4)
 #optimizer = torch.optim.Adam(decoder.parameters(), lr=1e-3)
-optimizerD = torch.optim.Adam(discriminstor.parameters(), lr=1e-3)
+#optimizerD = torch.optim.Adam(discriminator.parameters(), lr=1e-3)
 
 def save_image(data, file_name):
     data = data.astype(np.int8)
@@ -153,38 +255,40 @@ def save_image(data, file_name):
 
 def np_to_torch(data):
     """Convert to NCHW format"""
-    data = torch.from_numpy(data)
+    data = torch.tensor(data)
     data = data.permute(2,0,1)
     data = data.unsqueeze(0)
+    data = data.float().cuda()/255
     return data
 
 def torch_to_np(data):
     """Convert to NHWC format"""
     data = data.permute(0,2,3,1)
     data = data.cpu().numpy()
+    data *= 255
     return data
 
 def test_batch():
-    batch_size = 100
-    total_loss = 0
-    pbar = tqdm(total=len(test_data),desc='Testing')
-    indices = set(list(range(len(test_data))))
-    while len(indices) > 0:
-        pbar.update(batch_size)
-        batch_indices = random.sample(indices,batch_size)
-        indices -= set(batch_indices)
-        sample_var = Variable(torch.zeros([batch_size,LATENT_SIZE]).float().cuda(), requires_grad=False)
-        batched_data = torch.zeros([batch_size,3,210,160]) # TODO: Replace with empty after updating pytorch
-        for i,di in enumerate(batch_indices):
-            batched_data[i,:,:,:] = np_to_torch(test_data[di])
-        img_var = Variable(batched_data.float().cuda()/255, requires_grad=False)
+    with torch.no_grad():
+        batch_size = 50
+        total_loss = 0
+        pbar = tqdm(total=len(test_data),desc='Testing')
+        indices = set(list(range(len(test_data))))
+        while len(indices) > 0:
+            pbar.update(batch_size)
+            batch_indices = random.sample(indices,batch_size)
+            indices -= set(batch_indices)
+            sample = torch.zeros([batch_size,LATENT_SIZE]).float().cuda()
+            batched_data = torch.empty([batch_size,3,210,160]).cuda()
+            for i,di in enumerate(batch_indices):
+                batched_data[i,:,:,:] = np_to_torch(test_data[di])
 
-        o,m,lv = encoder(img_var, sample_var)
-        o = decoder(o)
-        l = loss(img_var, o, m, lv)
-        total_loss += l.data[0]*batch_size
+            o,m,lv = encoder(batched_data, sample)
+            o = decoder(o)
+            l = loss(batched_data, o, m, lv)
+            total_loss += l.item()*batch_size
 
-    return total_loss
+        return total_loss
 
 latents = []
 output = []
@@ -199,29 +303,35 @@ def save_examples(file_name):
     output = []
     inputs = []
     samples = []
-    for d in test_data[:10]+data[:10]:
-        img_var = Variable(np_to_torch(d).float().cuda()/255, requires_grad=False)
-        sample_var = Variable(torch.zeros([LATENT_SIZE]).float().cuda(), requires_grad=False)
-        o,m,lv = encoder(img_var, sample_var)
-        inputs.append(img_var)
-        samples.append(sample_var)
-        latents.append(o)
-        o = decoder(o)
-        o *= 255
-        output.append(torch_to_np(o.data)[0])
+    with torch.no_grad():
+        for i,d in enumerate(test_data[:10]+data[:10]):
+            img = np_to_torch(d)
+            sample = torch.zeros([LATENT_SIZE]).float().cuda()
+            o,m,lv = encoder(img, sample)
+            inputs.append(img)
+            samples.append(sample)
+            latents.append(o.data.cpu().numpy())
+            if i == 0:
+                o[0,0] = 0
+                o[0,1] = 1
+            if i == 1:
+                o[0,0] = 1
+                o[0,1] = 0
+            o = decoder(o)
+            output.append(torch_to_np(o.data)[0])
     test_examples = np.concatenate(output[:10],axis=1)
     train_examples = np.concatenate(output[10:],axis=1)
     all_examples = np.concatenate((test_examples,train_examples),axis=0)
     save_image(all_examples, file_name)
+    print(np.mean(np.array(latents),axis=0))
 
 def save_truth(file_name):
     latents = []
     output = []
-    for d in test_data[:10]+data[:10]:
-        #img_var = Variable(np_to_torch(d).cuda()/255, requires_grad=False)
-        img_var = Variable(np_to_torch(d).float().cuda()/255, requires_grad=False)
-        img_var *= 255
-        output.append(torch_to_np(img_var.data)[0])
+    with torch.no_grad():
+        for d in test_data[:10]+data[:10]:
+            img = np_to_torch(d)
+            output.append(torch_to_np(img.data)[0])
     test_examples = np.concatenate(output[:10],axis=1)
     train_examples = np.concatenate(output[10:],axis=1)
     all_examples = np.concatenate((test_examples,train_examples),axis=0)
@@ -237,13 +347,13 @@ batch_size = 1
 while True:
     file_name = "output/img-%d.png" % iters
     save_examples(file_name)
-    tqdm.write('Saved file: %s' % file_name)
+    print('Saved file: %s' % file_name)
     tl = test_batch()
-    tqdm.write('Test loss: %s' % tl)
+    print('Test loss: %s' % tl)
     iters += 1
 
     indices = set(list(range(len(data))))
-    pbar = tqdm(total=len(indices))
+    pbar = tqdm(total=len(indices), desc='Training')
     training_loss = 0
     while len(indices) > 0:
         pbar.update(batch_size)
@@ -251,20 +361,25 @@ while True:
         indices -= set(batch_indices)
 
         sample = np.random.normal(0,1,(batch_size,LATENT_SIZE))
-        sample_var = Variable(torch.from_numpy(sample).float().cuda(), requires_grad=False)
-        #sample_var = Variable(torch.zeros([batch_size,LATENT_SIZE]).float().cuda(), requires_grad=False)
+        sample = torch.tensor(sample, requires_grad=False).float().cuda()
+        #sample = torch.zeros([batch_size,LATENT_SIZE], requires_grad=False).float().cuda()
 
-        batched_data = torch.zeros([batch_size,3,210,160]) # TODO: Replace with empty after updating pytorch
+        batched_data = torch.empty([batch_size,3,210,160], requires_grad=False).cuda()
+        batched_latent = torch.zeros([batch_size,LATENT_SIZE], requires_grad=False).cuda()
         for i,di in enumerate(batch_indices):
             batched_data[i,:,:,:] = np_to_torch(data[di])
-        img_var = Variable(batched_data.float().cuda()/255, requires_grad=False)
+            batched_latent[i,di%2] = 1
 
         optimizer.zero_grad()
-        o,m,lv = encoder(img_var, sample_var)
+        o,m,lv = encoder(batched_data, sample)
         o = decoder(o)
-        l = loss(img_var, o, m, lv)
-        training_loss += l.data[0]
+        l = loss(batched_data, o, m, lv)
+        training_loss += l.item()
+
+        o = decoder(batched_latent)
+        l += loss(batched_data, o, m, lv)
 
         l.backward()
         optimizer.step()
-    tqdm.write('Training loss: %s' % training_loss)
+    pbar.close()
+    print('Training loss: %s' % training_loss)
